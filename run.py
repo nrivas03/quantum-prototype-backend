34334b911f7d95312212965e6004f2bbbb3b6985
nrivas03 <n.rivas03@ufromail.cl>
2024-12-13 07:33:49 -0300

    Enable CORS for the entire application

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from classic_node import classic_bp, quantum_integration_bp
 
 def create_app():
@@ -6,6 +7,9 @@ def create_app():
     Crea e inicializa la aplicación Flask.
     """
     app = Flask(__name__)
+
+    # Habilitar CORS para toda la aplicación
+    CORS(app, resources={r"/*": {"origins": "*"}})
 
     # Registrar Blueprints
     app.register_blueprint(classic_bp, url_prefix='/classic')
